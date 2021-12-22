76838fb3e7dbf628c403264bf3f5db1e42f72835
tracksuite <youngunner1998@gmail.com>
2021-12-22 11:53:09 +0530

    created SriLankan Flag

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,9 +1,17 @@
  precision mediump float;
 
+ uniform vec3 uColor;
+ uniform sampler2D uTexture;
+
+varying vec2 vUv;
+varying float vElevation;
 //  varying float vRandom;
 
     void main()
     {
+
+        vec4 textureColor = texture2D(uTexture, vUv);
+        textureColor.rgb += vElevation * 1.5 + 0.02;
         // vec4(r,g,b and a)
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = textureColor;
     }
